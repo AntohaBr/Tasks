f8914ec8deec5e9e28ddecd9ee3748a648cb9ca4
AntohaBr <brelanton@gmail.com>
2022-11-03 15:54:02 +0300

    HW13-fetch

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -39,20 +39,9 @@ function HW4() {
 
                 {/*----------------------------------------------------*/}
 
-                <SuperButton>
-                    default
-                </SuperButton>
-
-                <SuperButton
-                    red // пропсу с булевым значением не обязательно указывать true
-                    onClick={showAlert}
-                >
-                    delete {/*// название кнопки попадёт в children*/}
-                </SuperButton>
-
-                <SuperButton disabled>
-                    disabled
-                </SuperButton>
+                <SuperButton>default</SuperButton>
+                <SuperButton red onClick={showAlert}>delete</SuperButton>
+                <SuperButton disabled>disabled</SuperButton>
 
                 {/*----------------------------------------------------*/}
 
